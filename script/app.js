7977086b478ae2883467589b9e13e414d3c78828
bindupothana <himabindu.pothana@navaratan.com>
2017-03-23 19:18:44 +0530

    Factorial of  a  given number

--- script/app.js
+++ script/app.js
@@ -1,11 +1,13 @@
 var app=angular.module('factApp',[]);
 app.controller('fibCtrl',function($scope){
 	$scope.factorial=function(num){
-		var fact=1
-		for (var i=0;i<=$scope.num;i++)
-			console.log("an",fact)
-			fact=fact*i
+		 $scope.fact=1
+		for (var i=1;i<=$scope.num;i++){
+			$scope.fact=$scope.fact*i;
+			// $scope.result.push(fact)
+			console.log("result",$scope.fact)
 
-	}
+	    }
+    }
 
 });
